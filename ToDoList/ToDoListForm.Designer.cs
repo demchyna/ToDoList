ddc2e371ef03cbb34f21ba275103517c0aab8c15
demchyna <kdemchyna@gmail.com>
2018-04-17 01:14:40 +0300

    Initial commit

---
+++ ToDoList/ToDoListForm.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace ToDoList
+{
+    partial class ToDoListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ToDoListForm));
+            this.addButton = new System.Windows.Forms.Button();
+            this.upButton = new System.Windows.Forms.Button();
+            this.downButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.todoPanel = new System.Windows.Forms.Panel();
+            this.todoListBox = new System.Windows.Forms.CheckedListBox();
+            this.addLabel = new System.Windows.Forms.Label();
+            this.addTextBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showLabel = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.todoPanel.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(197, 35);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // upButton
+            // 
+            this.upButton.Location = new System.Drawing.Point(200, 15);
+            this.upButton.Name = "upButton";
+            this.upButton.Size = new System.Drawing.Size(57, 23);
+            this.upButton.TabIndex = 2;
+            this.upButton.Text = "Up";
+            this.upButton.UseVisualStyleBackColor = true;
+            this.upButton.Click += new System.EventHandler(this.UpButton_Click);
+            // 
+            // downButton
+            // 
+            this.downButton.Location = new System.Drawing.Point(200, 44);
+            this.downButton.Name = "downButton";
+            this.downButton.Size = new System.Drawing.Size(57, 23);
+            this.downButton.TabIndex = 3;
+            this.downButton.Text = "Down";
+            this.downButton.UseVisualStyleBackColor = true;
+            this.downButton.Click += new System.EventHandler(this.DownButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(200, 95);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(57, 23);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // todoPanel
+            // 
+            this.todoPanel.Controls.Add(this.todoListBox);
+            this.todoPanel.Controls.Add(this.upButton);
+            this.todoPanel.Controls.Add(this.downButton);
+            this.todoPanel.Controls.Add(this.deleteButton);
+            this.todoPanel.Location = new System.Drawing.Point(12, 64);
+            this.todoPanel.Name = "todoPanel";
+            this.todoPanel.Size = new System.Drawing.Size(260, 138);
+            this.todoPanel.TabIndex = 5;
+            // 
+            // todoListBox
+            // 
+            this.todoListBox.FormattingEnabled = true;
+            this.todoListBox.Location = new System.Drawing.Point(5, 6);
+            this.todoListBox.Name = "todoListBox";
+            this.todoListBox.Size = new System.Drawing.Size(189, 124);
+            this.todoListBox.TabIndex = 5;
+            this.todoListBox.SelectedIndexChanged += new System.EventHandler(this.TodoListBox_SelectedIndexChanged);
+            // 
+            // addLabel
+            // 
+            this.addLabel.AutoSize = true;
+            this.addLabel.Location = new System.Drawing.Point(12, 40);
+            this.addLabel.Name = "addLabel";
+            this.addLabel.Size = new System.Drawing.Size(34, 13);
+            this.addLabel.TabIndex = 6;
+            this.addLabel.Text = "Task:";
+            // 
+            // addTextBox
+            // 
+            this.addTextBox.Location = new System.Drawing.Point(45, 37);
+            this.addTextBox.Name = "addTextBox";
+            this.addTextBox.Size = new System.Drawing.Size(146, 20);
+            this.addTextBox.TabIndex = 7;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuItem,
+            this.helpMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileMenuItem
+            // 
+            this.fileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openMenuItem,
+            this.saveMenuItem,
+            this.toolStripSeparator1,
+            this.exitMenuItem});
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileMenuItem.Text = "File";
+            // 
+            // openMenuItem
+            // 
+            this.openMenuItem.Name = "openMenuItem";
+            this.openMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openMenuItem.Text = "Open";
+            // 
+            // saveMenuItem
+            // 
+            this.saveMenuItem.Name = "saveMenuItem";
+            this.saveMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveMenuItem.Text = "Save";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(100, 6);
+            // 
+            // exitMenuItem
+            // 
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitMenuItem.Text = "Exit";
+            // 
+            // helpMenuItem
+            // 
+            this.helpMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutMenuItem});
+            this.helpMenuItem.Name = "helpMenuItem";
+            this.helpMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpMenuItem.Text = "Help";
+            // 
+            // aboutMenuItem
+            // 
+            this.aboutMenuItem.Name = "aboutMenuItem";
+            this.aboutMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.aboutMenuItem.Text = "Abaut To-Do List";
+            // 
+            // showLabel
+            // 
+            this.showLabel.AutoSize = true;
+            this.showLabel.Location = new System.Drawing.Point(14, 205);
+            this.showLabel.MaximumSize = new System.Drawing.Size(260, 30);
+            this.showLabel.Name = "showLabel";
+            this.showLabel.Size = new System.Drawing.Size(0, 13);
+            this.showLabel.TabIndex = 9;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 239);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 10;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // ToDoListForm
+            // 
+            this.AcceptButton = this.addButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.showLabel);
+            this.Controls.Add(this.addTextBox);
+            this.Controls.Add(this.addLabel);
+            this.Controls.Add(this.todoPanel);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ToDoListForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "To-Do List App";
+            this.todoPanel.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button upButton;
+        private System.Windows.Forms.Button downButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Panel todoPanel;
+        private System.Windows.Forms.CheckedListBox todoListBox;
+        private System.Windows.Forms.Label addLabel;
+        private System.Windows.Forms.TextBox addTextBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutMenuItem;
+        private System.Windows.Forms.Label showLabel;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+    }
+}
+
---
+++ ToDoList/ToDoListForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class ToDoListForm : Form
+    {
+        public ToDoListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            statusLabel.Text = "";
+            string item = addTextBox.Text.Trim();
+            if (!item.Equals(""))
+            {
+                todoListBox.Items.Add(item);
+            }
+            else
+            {
+                statusLabel.Text = "The field 'Task' cannot be empty!";
+            }
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            statusLabel.Text = "";
+            object item = todoListBox.SelectedItem;
+            if (item != null)
+            {
+                todoListBox.Items.Remove(item);
+            }
+            else
+            {
+                statusLabel.Text = "The task from To-Do list wasn't selected!";
+            }
+            showLabel.Text = "";
+        }
+
+        private void UpButton_Click(object sender, EventArgs e)
+        {
+            object item = todoListBox.SelectedItem;
+
+            if (item != null)
+            {
+                int index = todoListBox.Items.IndexOf(item);
+
+                if (index != 0)
+                {
+                    bool isChecked = todoListBox.GetItemChecked(index);
+
+                    todoListBox.Items.Remove(item);
+                    todoListBox.Items.Insert(index - 1, item);
+
+                    todoListBox.SelectedItem = item;
+                    todoListBox.SetItemChecked(index - 1, isChecked);
+                }
+                else
+                {
+                    statusLabel.Text = "The task is in the top!";
+                }
+            }
+            else
+            {
+                statusLabel.Text = "The task from To-Do list wasn't selected!";
+            }
+        }
+
+        private void DownButton_Click(object sender, EventArgs e)
+        {
+            object item = todoListBox.SelectedItem;
+
+            if (item != null)
+            {
+                int index = todoListBox.Items.IndexOf(item);
+
+                if (index != todoListBox.Items.Count - 1)
+                {
+                    bool isChecked = todoListBox.GetItemChecked(index);
+
+                    todoListBox.Items.Remove(item);
+                    todoListBox.Items.Insert(index + 1, item);
+
+                    todoListBox.SelectedItem = item;
+                    todoListBox.SetItemChecked(index + 1, isChecked);
+                }
+                else
+                {
+                    statusLabel.Text = "The task is in the bottom!";
+                }
+            }
+            else
+            {
+                statusLabel.Text = "The task from To-Do list wasn't selected!";
+            }
+        }
+
+        private void TodoListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            object item = todoListBox.SelectedItem;
+
+            if (item != null)
+            {
+                showLabel.Text = item.ToString();
+            }
+        }
+    }
+}
